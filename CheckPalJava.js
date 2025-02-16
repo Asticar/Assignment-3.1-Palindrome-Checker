6cdf6ffe8f5392ac12b8686f4c330e1e98f340d0
Asticar <147846659+Asticar@users.noreply.github.com>
2025-02-16 01:26:45 -0600

    Add files via upload

--- CheckPalJava.js
+++ CheckPalJava.js
@@ -1,14 +1,15 @@
 function checkPal()
 {
+    // creates a variable for the initial string that was input by the user
     var string1 = document.getElementById("initString").value;
-    
+    // will effectively split the string apart, letter by letter (I.E: aeiou becomes a e i o u)
     var splitString = string1.split("");
-    
+    // will flip the string in reverse (I.E. a e i o u becomes u o i e a)
     var revString = splitString.reverse();
-
+    // will join the string back together (I.E. u o i e a becomes uoiea)
     var joinString = revString.join("");
     
-
+    // checks to see if the original string that was input is the same as the reversed version of the string
     if (string1 == joinString)
     {
         document.getElementById("results").innerHTML = "Is a palindrome";
